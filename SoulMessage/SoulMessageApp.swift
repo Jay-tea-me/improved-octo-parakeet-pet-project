bf047cce5bb5ea6470481d7a0a3dc9f5485ebba5
justine wright <wright.justine21@gmail.com>
2021-07-06 12:39:28 +0200

    Initial Commit

---
+++ SoulMessage/SoulMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulMessageApp.swift
+//  SoulMessage
+//
+//  Created by Justine Wright on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SoulMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
