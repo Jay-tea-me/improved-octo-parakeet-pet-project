14164a55fa95160f6861a14313e0f190fac40b5e
justine wright <wright.justine21@gmail.com>
2021-07-07 14:29:43 +0200

    - added repository protocol

---
+++ SoulMessage/Services/RepositoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  RepositoryProtocol.swift
+//  SoulMessage
+//
+//  Created by Justine Wright on 2021/07/06.
+//
+
+import Foundation
+
+protocol RepositoryProtocol {
+    
+    func get()
+    
+    func add()
+}
