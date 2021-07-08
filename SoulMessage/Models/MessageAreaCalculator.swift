da49280fe8bc733a485284bb26da6ed0aa252e2b
justine wright <wright.justine21@gmail.com>
2021-07-08 21:26:10 +0200

    - added test structure

---
+++ SoulMessage/Models/MessageAreaCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  MessageAreaCalculator.swift
+//  SoulMessage
+//
+//  Created by Justine Wright on 2021/07/08.
+//
+
+import Foundation
+
+struct MessageAreaCalculator {
+    private let range: Double = 1.0
+    
+    
+    
+}
---
+++ SoulMessageTests/SoulMessageTests.swift
@@ -0,0 +1,52 @@
+//
+//  SoulMessageTests.swift
+//  SoulMessageTests
+//
+//  Created by Justine Wright on 2021/07/08.
+//
+
+import XCTest
+
+class SoulMessageTests: XCTestCase {
+    
+    func testLatitudeLowerBoundIsCurrentLocationMinusRange(){
+        
+    }
+    
+    func testLatitudeUpperBoundIsCurrentLocationPlusRange(){
+        
+    }
+    
+    func testLongitudeLowerBoundIsCurrentLocationMinusRange(){
+        
+    }
+    
+    func testLongitudeUpperBoundIsCurrentLocationPlusRange(){
+        
+    }
+    
+    func testCurrentLongitudeInBounds(){
+        
+    }
+    
+    func testCurrentLatitudeInBounds(){
+        
+    }
+    
+    func testNewUpperBoundLongitudeInBounds(){
+        
+    }
+    
+    func testNewLowerBoundLongitudeInBounds(){
+        
+    }
+    
+    func testNewUpperBoundLatitudeInBounds(){
+        
+    }
+    
+    func testNewLowerBoundLatitudeInBounds(){
+        
+    }
+
+}
