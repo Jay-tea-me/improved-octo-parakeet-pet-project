eb7aef60755fd083f9966183df7f484e374cb1b7
justine wright <wright.justine21@gmail.com>
2021-07-23 09:59:12 +0200

    adds files - view controller and view model

---
+++ SoulMessage/ViewModels/FormViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FormViewModel.swift
+//  SoulMessage
+//
+//  Created by Justine Wright on 2021/07/23.
+//
+
+import Foundation
---
+++ SoulMessage/Views/Messaging/FormViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FormViewController.swift
+//  SoulMessage
+//
+//  Created by Justine Wright on 2021/07/23.
+//
+
+import Foundation
